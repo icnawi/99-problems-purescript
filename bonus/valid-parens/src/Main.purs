e308914c2db7b71ff308bfa7e717d65f7b082d14
icnawi <icnawi@outlook.com>
2026-06-05 22:59:47 -0700

    valid parenthesis - untested

--- bonus/valid-parens/src/Main.purs
+++ bonus/valid-parens/src/Main.purs
@@ -8,4 +8,8 @@ import Parens (isValidParens)
 
 main :: Effect Unit
 main = do
-  log $ show $ isValidParens "(){}[]"
+  log $ show $ isValidParens "(){}[]<>"
+  log $ show $ isValidParens "([])"
+  log $ show $ isValidParens "(]"
+  log $ show $ isValidParens "()"
+  log $ show $ isValidParens "([)]"
--- bonus/valid-parens/src/Parens.purs
+++ bonus/valid-parens/src/Parens.purs
@@ -1,21 +1,29 @@
-module Parens where
+module Parens
+  ( isValidParens
+  , strToListChar
+  )
+  where
 
 import Prelude
 
-import Data.Foldable(lookup)
-import Data.Maybe(Maybe(..))
-import Data.List(List(..), null, (:))
-import Data.Tuple(Tuple(..))
+import Data.Foldable (lookup)
+import Data.List (List(..), fromFoldable, null, (:))
+import Data.Maybe (Maybe(..))
+import Data.String.CodeUnits (toCharArray)
+import Data.Tuple (Tuple(..))
 
 
--- isValidParens :: String -> Boolean
-isValidParens xs = isBalanced xs Nil
+isValidParens :: String -> Boolean
+isValidParens xs = isBalanced (strToListChar xs) Nil
 
--- isBalanced :: String -> String -> Boolean
+strToListChar ∷ String →  List Char
+strToListChar xs = fromFoldable $ toCharArray xs
+
+isBalanced :: List Char -> List Char -> Boolean
 isBalanced Nil x = null x
 
 isBalanced (c:xs) ys 
-  | Just d <- lookup c [ Tuple '(' ')', Tuple '[' ']', Tuple '{' '}' ] = isBalanced xs (Cons d ys)
+  | Just d <- lookup c [ Tuple '(' ')', Tuple '[' ']', Tuple '{' '}', Tuple '<' '>' ] = isBalanced xs (d:ys)
 
 isBalanced _ Nil = false
 isBalanced (x:xs) (y:ys) = x == y && isBalanced xs ys
