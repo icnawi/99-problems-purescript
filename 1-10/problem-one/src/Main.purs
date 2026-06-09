a69f8408ee5e41331e216ee9f9dd98c50f329b0f
icnawi <icnawi@outlook.com>
2026-06-09 16:43:46 -0700

    problem 1: V1 -> arrays + linked lists

--- 1-10/problem-one/src/Main.purs
+++ 1-10/problem-one/src/Main.purs
@@ -3,16 +3,22 @@ module Main where
 import Prelude
 
 import Data.Array.Partial (tail)
+import Data.List (List(..), (:))
 import Effect (Effect)
 import Effect.Console (log)
 import Partial.Unsafe (unsafeCrashWith, unsafePartial)
 
 main ∷ Effect Unit
 main = do
-  let numbers = [1, 2, 3, 4]
-  log $ show $ unsafePartial myLast numbers
+  log $ show $ unsafePartial myLast [10000, 23000, 42]
+  log $ show $ unsafePartial myLast' (124 : 52 : 4 : Nil)
 
 myLast ∷ forall a. Partial => Array a -> a
-myLast [] = unsafeCrashWith "No end for empty lists!"
+myLast [] = unsafeCrashWith "No end for empty arrays!"
 myLast [x] = x
 myLast xs = myLast (tail xs)
+
+myLast' ∷ forall a. Partial => List a -> a
+myLast' Nil = unsafeCrashWith "No end for empty lists!"
+myLast' (x : Nil) = x
+myLast' (_:xs) = myLast' xs
--- 1-10/problem-one/test/Main.purs
+++ 1-10/problem-one/test/Main.purs
@@ -2,8 +2,9 @@ module Test.Main where
 
 import Prelude
 
+import Data.List (List(..), (:))
 import Effect (Effect)
-import Main (myLast)
+import Main (myLast, myLast')
 import Partial.Unsafe (unsafePartial)
 import Test.Unit (suite, test)
 import Test.Unit.Assert as Assert
@@ -18,5 +19,19 @@ main = runTest do
     test "Chars" do
       Assert.assert "['x', 'y', 'z'] should be 'z'" $ unsafePartial (myLast ['x', 'y', 'z']) == 'z'
       Assert.equal 'z' (unsafePartial (myLast ['x','y','z']))
+    test "Strings" do
+      Assert.assert "[\"apple\", \"banana\", \"cherry\"] should be \"cherry\"" $ unsafePartial (myLast ["apple", "banana", "cherry"]) == "cherry"
+      Assert.equal "cherry" (unsafePartial (myLast ["apple", "banana", "cherry"]))
+
+  suite "The last element of Linked List" do
+    test "Integers" do
+      Assert.assert "(1 : 2 : 3 : 4 : Nil) should be 4" $ unsafePartial (myLast' (1 : 2 : 3 : 4 : Nil)) == 4
+      Assert.equal 4 (unsafePartial (myLast' (1 : 2 : 3 : 4 : Nil)))
+    test "Chars" do
+      Assert.assert "('x' : 'y' : 'z' : Nil) should be 'z'" $ unsafePartial (myLast' ('x' : 'y' : 'z' : Nil)) == 'z'
+      Assert.equal 'z' (unsafePartial (myLast' ('x' : 'y' : 'z' : Nil)))
+    test "Strings" do
+      Assert.assert "(\"apple\" : \"banana\" : \"cherry\" : Nil) should be \"cherry\"" $ unsafePartial (myLast' ("apple" : "banana" : "cherry" : Nil)) == "cherry"
+      Assert.equal "cherry" (unsafePartial (myLast' ("apple" : "banana" : "cherry" : Nil)))
 
 
